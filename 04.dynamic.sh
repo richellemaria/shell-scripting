6c46790c96be4e4ddc252a1e1a4eec5521070823
richellemaria <rishdsouza@gmail.com>
2023-06-03 14:18:32 +0530

    auto push

--- 04.dynamic.sh
+++ 04.dynamic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #Today_date="03062023"    #hardcoded
-Today_date=$(date +%D)    #softcode
+Today_date=$(date +%D)    #softcode expression always in paranthesis
 
 echo -e "Today's date is \e[34m ${Today_date} \e[0m"
