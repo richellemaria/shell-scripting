43864b56bf51b7a4ec2298dbdcf617ed22a96c04
richellemaria <rishdsouza@gmail.com>
2023-06-03 19:03:20 +0530

    auto push

--- 05.specialvariable.sh
+++ 05.specialvariable.sh
@@ -7,3 +7,10 @@ echo $0    #prints script name you executing
 echo "Name of script executing is $0"
 echo "My name is is $1"
 echo "I am studying $2"
+
+
+echo $*
+echo $@
+echo $$
+echo $#
+echo $?
