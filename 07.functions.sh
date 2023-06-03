22dc759bfeabc15d2a305715354253356d5b801c
richellemaria <rishdsouza@gmail.com>
2023-06-03 19:52:03 +0530

    auto push

--- 07.functions.sh
+++ 07.functions.sh
@@ -18,6 +18,7 @@ status(){
     no_user=$(who | wc -l)
     echo "Today's date is $(date +%F)"
     echo  -e "Number of user running \e[43m $no_user \e[0m"
+    echo -e "The load average for last 1 min $(uptime | awk -F, '{print $3}' | awk -F: '{print $2}')
 }
 
 #calling status function
