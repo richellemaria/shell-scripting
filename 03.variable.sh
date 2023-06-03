e8959ae85749720378e80a3bd11cf3cff3ca46dd
richellemaria <rishdsouza@gmail.com>
2023-06-03 14:02:11 +0530

    auto push

--- 03.variable.sh
+++ 03.variable.sh
@@ -15,6 +15,6 @@ c=30
 echo $a
 echo The value of variable b is $b
 echo The value of variable c is ${c}
-echo -e "The value of the varaible a is \e[45m \e[0m"
+echo -e "The value of the varaible a is \e[45m $a \e[0m"
 
 echo "The value of variable d is ${d}"
