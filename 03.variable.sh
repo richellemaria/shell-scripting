1a4b5b98d18fb58d2ebbae042765f1d3c8ff7534
richellemaria <rishdsouza@gmail.com>
2023-06-03 13:12:23 +0530

    auto push

---
+++ 03.variable.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# variable is used to store value
+
+a=10
+name=richelle
+topic=shell scripting
+
+#declare variable
+
+a=10
+b=20
+c=30
+
+echo $a
+echo The value of variable b is $b
