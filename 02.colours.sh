bfde4739895b0b4a36deb7d3604291488e477cf6
richellemaria <rishdsouza@gmail.com>
2023-06-03 12:50:15 +0530

    auto push

--- 02.colours.sh
+++ 02.colours.sh
@@ -7,4 +7,4 @@ echo -e "\e[34m I am prininting blue colour \e[0m"
 
 #printing background clour
 
-echo -e "\e[43;32m I am printing background clour \e[0m'
+echo -e "\e[43;32m I am printing background clour \e[0m"
