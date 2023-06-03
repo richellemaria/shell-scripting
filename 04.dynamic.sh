5c36ad5e8689fbd3d7c7a3e0d2ff1fe725679db5
richellemaria <rishdsouza@gmail.com>
2023-06-03 14:24:25 +0530

    auto push

--- 04.dynamic.sh
+++ 04.dynamic.sh
@@ -2,7 +2,7 @@
 
 #Today_date="03062023"    #hardcoded
 Today_date=$(date +%D)    #softcode expression always in paranthesis
-No_user=%(who | wc -l)    #who shows no of session or user logged in
+No_user=$(who | wc -l)    #who shows no of session or user logged in
 
 echo -e "Today's date is \e[34m ${Today_date} \e[0m"
-echo -e "The no of user are \e[32m %{No_user}\e[0m"
+echo -e "The no of user are \e[32m ${No_user} \e[0m"
