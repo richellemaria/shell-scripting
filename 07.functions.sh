67ca270e06c2193fa1b58a662666c33002678f4c
richellemaria <rishdsouza@gmail.com>
2023-06-03 19:24:15 +0530

    auto push

---
+++ 07.functions.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+#declare a function
+
+sample(){
+
+    echo "I am sample function with name sample"
+    echo "I am executing sample function
+}
+
+#calling sample function
+
+sample
+status
+
+#declare status function
+
+status(){
+    echo "Today's date is $(date +%F)"
+    echo "Number of process running $(who | wc -l)"
+}
+
+#calling status function
+
+status
