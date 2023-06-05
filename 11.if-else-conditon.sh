b6f08697736b17421c608742cb9584d969aed077
richellemaria <rishdsouza@gmail.com>
2023-06-05 23:18:48 +0530

    auto push

--- 11.if-else-conditon.sh
+++ 11.if-else-conditon.sh
@@ -4,14 +4,14 @@ action=$1
 if [ "$action" == "start" ] ; then
    echo -e "\e[34m starting server \e[0m"
    exit 0
-elif [ "$action" == "stop" ]; then
+elif [ "$action" == "stop" ] ; then
     echo -e "\e[32m stopping server \e[0m"
     exit 1
-elif [ "$action" == "restart" ]; then
+elif [ "$action" == "restart" ] ; then
     echo -e "\e[31m restarting server \e[0m"
     exit 2
 else 
-    echo "file executed $0"
+    echo -e "file executed $0"
     exit 3
 
 fi
