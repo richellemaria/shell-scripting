98f777c8293547cc44df7a36f786bd433cf38b1e
richellemaria <rishdsouza@gmail.com>
2023-06-05 23:15:33 +0530

    auto push

--- 11.if-else-conditon.sh
+++ 11.if-else-conditon.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 action=$1
-if ["$action" == "start"]; then
+if ["$action" == "start"] ; then
    echo -e "\e[34m starting server \e[0m"
    exit 0
 elif ["$action" == "stop"]; then
     echo -e "\e[32m stopping server \e[0m"
     exit 1
-elif ["$action"=="restart"]; then
+elif ["$action" == "restart"]; then
     echo -e "\e[31m restarting server \e[0m"
     exit 2
 else 
