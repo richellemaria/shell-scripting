ce3d2ba8de08713779aad73f9096afac295c9aff
richellemaria <rishdsouza@gmail.com>
2023-06-06 22:32:41 +0530

    auto push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -36,3 +36,8 @@ mv static/* .
 rm -rf ${COMPONENT}-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf 
 stat $?
+
+echo -n "starting ${COMPONENT} service"
+systemctl enable nginx
+systemctl start nginx
+stat $?
