b016a7cda896b11b007147a7f8a7708f813f8e30
richellemaria <rishdsouza@gmail.com>
2023-06-06 22:44:56 +0530

    auto push

---
+++ roboshop/components/Mongodb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+COMPONENT=mongodb
+LOGFILE=/tmp/${COMPONENT}.log
+ID=$(id -u)
+if [ $ID -ne 0 ]; then
+   echo -e "\e[31m This should be run as root or sudo previlige \e[0m"
+   exit 1
+fi
+
+stat(){
+if [ $1 -eq 0 ]; then
+  echo -e "\e[32m success \e[0m"
+else 
+  echo -e "\e[31m failure \e[0m"
+fi
+}
+
+echo -n "configuring the repo"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+stat $?
