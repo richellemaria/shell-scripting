ccf8cd4e253359375fa428861259b8529a57d306
richellemaria <rishdsouza@gmail.com>
2023-06-06 20:28:46 +0530

    auto push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+COMPONENT=frontend
 ID=$(id -u)
-if [ $ID -ne 0]; then
+if [ $ID -ne 0 ]; then
    echo -e "\e[31m This should be run as root or sudo previlige \e[0m"
    exit 1
 fi
 
 echo "installing nginx"
-yum install nginx -y
+yum install nginx -y &>> "/tmp/${COMPONENT}.log"
