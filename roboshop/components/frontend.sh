417968b1dbbfbbcbb746631b112c04736a7895c7
richellemaria <rishdsouza@gmail.com>
2023-06-06 20:08:45 +0530

    auto push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -2,7 +2,7 @@
 
 ID=$(id -u)
 if [ $ID -neq 0]; then
-   echo -e "\e[31m This should be run as root or sudo previlige ]
+   echo -e "\e[31m This should be run as root or sudo previlige \e[0m"
    exit 1
 fi
 
