5950e763a029e8efe99a9163def845471c101545
richellemaria <rishdsouza@gmail.com>
2023-06-06 22:15:32 +0530

    auto push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -7,28 +7,24 @@ if [ $ID -ne 0 ]; then
    exit 1
 fi
 
+stat(){
+    if [ $1 -eq 0 ]; then
+  echo -e "\e[32m success \e[0m"
+else 
+  echo -e "\e[31m failure \e[0m"
+fi
+}
+
 echo -n "installing nginx"
 yum install nginx -y &>> "/tmp/${COMPONENT}.log"
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m success \e[0m"
-else 
-  echo -e "\e[31m failure \e[0m"
-fi
+stat $?
 
-echo -n "downloading the frontend component"
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m success \e[0m"
-else 
-  echo -e "\e[31m failure \e[0m"
-fi
+echo -n "downloading the ${COMPONENT} component"
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+stat $?
 
 echo -n "performing cleanup"
 cd /usr/share/nginx/html
 rm -rf * &>> "/tmp/${COMPONENT}.log"
 
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m success \e[0m"
-else 
-  echo -e "\e[31m failure \e[0m"
-fi
+stat $?
