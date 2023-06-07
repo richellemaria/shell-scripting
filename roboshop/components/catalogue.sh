0b77a9963881c37cc2f0a2ee76d1432871e0fba6
richellemaria <rishdsouza@gmail.com>
2023-06-07 09:17:07 +0530

    auto push

---
+++ roboshop/components/catalogue.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+COMPONENT=catalogue
+LOGFILE=/tmp/${COMPONENT}.log
+ID=$(id -u)
+if [ $ID -ne 0 ]; then
+   echo -e "\e[31m This should be run as root or sudo previlige \e[0m"
+   exit 1
+fi
+
+stat(){
+if [ $1 -eq 0 ]; then
+  echo -e "\e[32m success \e[0m"
+else 
+  echo -e "\e[31m failure \e[0m"
+  exit 2
+fi
+}
+
+echo -n "configuring the repo"
+curl curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+stat $?
