788604c0b7bffc884cdfe38569dc1e9fb6d74e39
richellemaria <rishdsouza@gmail.com>
2023-06-07 10:20:37 +0530

    auto push

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -20,3 +20,12 @@ fi
 echo -n "configuring the repo"
 curl curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>> $LOGFILE
 stat $?
+
+echo -n "installing nodejs"
+yum install nodejs -y &>> $LOGFILE
+stat $?
+
+echo -n "creating service account"
+useradd roboshop
+stat $?
+
