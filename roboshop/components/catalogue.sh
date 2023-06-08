16382388a3d1205cb385e94b32def8ea375d4318
richellemaria <rishdsouza@gmail.com>
2023-06-08 20:05:01 +0530

    auto push

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -40,7 +40,7 @@ stat $?
 
 echo -n "copying the $COMPONENT to $Appuser home directory"
 cd /home/$Appuser
-rm -f $COMPONENT &>> $LOGFILE
+rm -rf $COMPONENT &>> $LOGFILE
 unzip -o /tmp/$COMPONENT.zip &>> $LOGFILE
 stat $?
 
