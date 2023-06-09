c46c405b122d3f468584ca4108f9c1069cbe485f
richellemaria <rishdsouza@gmail.com>
2023-06-10 00:15:05 +0530

    auto push

--- roboshop/create-server.sh
+++ roboshop/create-server.sh
@@ -8,7 +8,13 @@
 
 #aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId'
 
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
-echo -e "AMI ID is to launch ec2 instance \e[34m $AMI_ID\e[0m"
+#aws ec2 describe-security-groups --filters Name=ip-permission.from-port,Values=22 Name=ip-permission.to-port,Values=22
 
+#aws ec2 describe-security-groups --filters Name=group-name,Values=b54-allow-all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g'
+
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
+SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b54-allow-all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
+
+echo -e "AMI ID is to launch ec2 instance \e[34m $AMI_ID\e[0m"
+echo -e "security group to launch ec2 instance \e[31m $SG_ID\e[0m"
 aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro
