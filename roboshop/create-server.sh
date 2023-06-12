68ac04d42759d38aaa2b80bb0dd552fd2eed1c9f
richellemaria <rishdsouza@gmail.com>
2023-06-12 08:03:57 +0530

    auto push

--- roboshop/create-server.sh
+++ roboshop/create-server.sh
@@ -34,5 +34,5 @@ echo -e "Private IP address to launch ec2 instance \e[34m $IP_ADD \e[0m"
 echo " launching is $COMPONENT is completed"
 
 echo -e "creating dns record $COMPONENT"
-sed -e "/s/COMPONENT/$COMPONENT/"  -e "/s/IPADD/$IP_ADD/" /roboshop/route53.json > /tmp/roboshop.json
+sed -e "/s/COMPONENT/${COMPONENT}/"  -e "/s/IPADD/${IP_ADD}/" /roboshop/route53.json > /tmp/roboshop.json
 aws route53 change-resource-record-sets --hosted-zone-id ${HostedZoneID} --change-batch file://route53.json
